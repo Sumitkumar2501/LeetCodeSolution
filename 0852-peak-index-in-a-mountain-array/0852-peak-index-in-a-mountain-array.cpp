56c1e72d316c12e716293f775b59b90b5693a300
Sumitkumar2501 <146692562+Sumitkumar2501@users.noreply.github.com>
2026-04-23 15:59:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,40 +1,60 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        if(arr.size()<3) return 0;
+        // if(arr.size()<3) return 0;
 
-        int res=-1;
-        int i=0;
-        bool peak=false;
-        bool valley=false;
-        int greater=arr[0];
+        // int res=-1;
+        // int i=0;
+        // bool peak=false;
+        // bool valley=false;
+        // int greater=arr[0];
 
-        while(i<arr.size()-1){
-            if(arr[i]<arr[i+1]){
-                int start=i;
-                while(i<arr.size()-1 && arr[i]<arr[i+1]){
-                    i++;
-                    peak=true;
-                }
-                while(i<arr.size()-1 && arr[i]>arr[i+1]){
-                    i++;
-                    valley=true;
-                }
-                if(peak==true && valley==true){
-                    for(int i=0;i<arr.size()-1;i++){
-                        if(arr[i]>greater){
-                            greater=arr[i];
-                            res=i;
-                        }
-                    }
-                }
-                peak=false;
-                valley=false;
+        // while(i<arr.size()-1){
+        //     if(arr[i]<arr[i+1]){
+        //         int start=i;
+        //         while(i<arr.size()-1 && arr[i]<arr[i+1]){
+        //             i++;
+        //             peak=true;
+        //         }
+        //         while(i<arr.size()-1 && arr[i]>arr[i+1]){
+        //             i++;
+        //             valley=true;
+        //         }
+        //         if(peak==true && valley==true){
+        //             for(int i=0;i<arr.size()-1;i++){
+        //                 if(arr[i]>greater){
+        //                     greater=arr[i];
+        //                     res=i;
+        //                 }
+        //             }
+        //         }
+        //         peak=false;
+        //         valley=false;
+        //     }
+        //     else{
+        //         i++;
+        //     }
+        // }
+        // return res;
+
+
+        //method 2
+
+        int st=1;
+        int end=arr.size()-2;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+
+            if(arr[mid-1]<arr[mid] && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            else if(arr[mid-1]>arr[mid]){
+                st=mid+1;
             }
             else{
-                i++;
+                end=mid-1;
             }
         }
-        return res;
+        return -1;
     }
 };
