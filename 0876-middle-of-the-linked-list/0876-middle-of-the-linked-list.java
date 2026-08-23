990ea1228ce5e8c8fe842fe152563027daf52ff1
Sumitkumar2501 <146692562+Sumitkumar2501@users.noreply.github.com>
2026-08-23 22:43:43 +0530

    Time: 0 ms (100%), Space: 42.9 MB (53.82%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -9,23 +9,36 @@
  * }
  */
 class Solution {
-    static int getLength(ListNode head){
-        int length=0;
+    // static int getLength(ListNode head){
+    //     int length=0;
 
-        while(head!=null){
-            length++;
-            head=head.next;
-        }
-        return length;
-    }
-    public ListNode middleNode(ListNode head) {
-        int len=getLength(head);
+    //     while(head!=null){
+    //         length++;
+    //         head=head.next;
+    //     }
+    //     return length;
+    // }
+    // public ListNode middleNode(ListNode head) {
+    //     int len=getLength(head);
 
-        int midIdx=len/2;
-        while(midIdx>0){
-            head=head.next;
-            midIdx--;
+    //     int midIdx=len/2;
+    //     while(midIdx>0){
+    //         head=head.next;
+    //         midIdx--;
+    //     }
+    //     return head;
+    // }
+
+    //second method
+
+    public ListNode middleNode(ListNode head){
+        ListNode slowptr=head;
+        ListNode fastptr=head;
+
+        while(fastptr!=null && fastptr.next!=null){
+            fastptr=fastptr.next.next;
+            slowptr=slowptr.next;
         }
-        return head;
+        return slowptr;
     }
 }
