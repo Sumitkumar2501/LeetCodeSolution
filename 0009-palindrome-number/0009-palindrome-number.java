8eff119b6a0766fb195ff0bc4cae92c71ee355c8
Sumitkumar2501 <146692562+Sumitkumar2501@users.noreply.github.com>
2026-09-04 23:41:11 +0530

    Time: 6 ms (21.19%), Space: 45.8 MB (71.79%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -7,18 +7,34 @@ class Solution {
 
         //2nd method
 
-        if(x<0){
-            return false;
-        }
+        // if(x<0){
+        //     return false;
+        // }
 
-        int original=x;
-        int reverse=0;
-        while(x!=0){
-            int digit=x%10;
-            reverse=reverse*10+digit;
-            x/=10;
-        }
+        // int original=x;
+        // int reverse=0;
+        // while(x!=0){
+        //     int digit=x%10;
+        //     reverse=reverse*10+digit;
+        //     x/=10;
+        // }
 
-        return original==reverse;
+        // return original==reverse;
+
+        //3rd method
+
+        String str=String.valueOf(x);
+
+        int left=0;
+        int right=str.length()-1;
+
+        while(left<right){
+            if(str.charAt(left)!=str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 }
